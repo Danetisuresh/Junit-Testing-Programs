44dd9f598a04d4360df473c0b4c5a64d12921b5a
suresh Daneti <sureshdaneti99@gmail.com>
2021-09-25 16:15:29 +0530

    Square Root of a non negative number

---
+++ SquareRoot.java
@@ -0,0 +1,27 @@
+package Com.Bridgelabz;
+import java.util.Scanner;
+public class SquareRoot {
+	public static Scanner sc =new Scanner(System.in);
+	public static void main(String[] args) {
+		sqrt();
+	}
+	public static void sqrt()
+	{
+		System.out.println("\n Enter the non negative number:");
+		int c=sc.nextInt();
+
+		if(c>0)
+		{
+			double epsilon = Math.pow(10, (-15));
+			double t = c;
+			while (Math.abs(t - c / t) > epsilon * t) {
+				t = (c / t + t) / 2.0;
+			}
+			System.out.println("\n The Absolute value is "+t);
+		}
+		else
+			System.out.println("\n you entered negative number!!!");
+	}
+
+
+}
